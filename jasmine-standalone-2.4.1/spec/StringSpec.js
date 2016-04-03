b8b58c83f28138d3540676ef87e25d75c6c61125
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-04-04 01:54:03 +0300

    Add hasVowels test suite

--- jasmine-standalone-2.4.1/spec/StringSpec.js
+++ jasmine-standalone-2.4.1/spec/StringSpec.js
@@ -1,5 +1,38 @@
 describe('Extended String Class', function() {
-  it('works', function(){
-    expect(2).toBe(2);
+  describe('method hasVowels', function() {
+    it('is defined', function() {
+      expect(String.hasVowels).toBeDefined();
+    });
+
+    it('is a function', function() {
+      expect(String.hasVowels).toEqual(jasmine.any(Function));
+    });
+
+    it('returns true if a string contains vowels', function() {
+      expect('an'.hasVowels()).toBe(true);
+      expect('egg'.hasVowels()).toBe(true);
+      expect('indy'.hasVowels()).toBe(true);
+      expect('on'.hasVowels()).toBe(true);
+      expect('up'.hasVowels()).toBe(true);
+      expect('cafè'.hasVowels()).toBe(true);
+      expect('an indy egg on up cafè'.hasVowels()).toBe(true);
+      expect('CASE'.hasVowels()).toBe(true);
+      expect('?/ENDS'.hasVowels()).toBe(true);
+      expect('WITH'.hasVowels()).toBe(true);
+      expect('ON'.hasVowels()).toBe(true);
+      expect('UP5'.hasVowels()).toBe(true);
+      expect('A Sentence Combining CasEs and +?\\ 4768'.hasVowels()).toBe(true);
+    });
+
+    it('returns false if a string does not contain vowels', function() {
+      expect('bcd'.hasVowels()).toBe(false);
+      expect('fgh ijk'.hasVowels()).toBe(false);
+      expect('LMN'.hasVowels()).toBe(false);
+      expect('pqr STV'.hasVowels()).toBe(false);
+      expect('wXyZ'.hasVowels()).toBe(false);
+      expect('!@#$%^&()_+?'.hasVowels()).toBe(false);
+      expect('2355805'.hasVowels()).toBe(false);
+      expect('áéîôü'.hasVowels()).toBe(false);
+    });
   });
 });
