f2b7733a6fb03251f7c443f9382ea10a4d5a0d4c
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-04-06 12:20:18 +0300

    Implelemt toUpper
    All tests pass

--- jasmine-standalone-2.4.1/src/String.js
+++ jasmine-standalone-2.4.1/src/String.js
@@ -3,3 +3,15 @@ String.prototype.hasVowels = function() {
   // i modifier makes it case insesitive
   return /[aeiou]/i.test(this.valueOf());
 };
+
+String.prototype.toUpper = function() {
+  // Difference between upper and lowercase characters in ASCII chart
+  var lowerUpperDifference = 'a'.charCodeAt() - 'A'.charCodeAt();
+
+  // Search for all lowercase characters and convert to uppercase equivalent
+  return this.replace(/[a-zà-ż]/g, function convert(character) {
+    // Convert to decimal, subtract difference to convert to uppercase
+    // then convert back to a character
+    return String.fromCharCode(character.charCodeAt() - lowerUpperDifference);
+  });
+};
