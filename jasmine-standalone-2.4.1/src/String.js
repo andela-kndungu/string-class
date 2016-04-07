bdc8faa025ee1d7fa89b1fe19b0c27336ed38654
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-04-07 10:51:03 +0300

    Add global flag as a precaution

--- jasmine-standalone-2.4.1/src/String.js
+++ jasmine-standalone-2.4.1/src/String.js
@@ -1,7 +1,7 @@
 String.prototype.hasVowels = function() {
   // Return true if a vowel is in the string
   // i modifier makes it case insesitive
-  return /[aeiou]/i.test(this.valueOf());
+  return /[aeiou]/ig.test(this.valueOf());
 };
 
 String.prototype.toUpper = function() {
